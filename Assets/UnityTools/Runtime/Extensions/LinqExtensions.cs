1edc6bfbf0b34c0d7d1a856f31552b8527a100b2
Famtosha <artem_kazakovtsev@mail.ru>
2022-09-01 11:21:10 +0500

    Updated and added LINQ extensions

--- Assets/UnityTools/Runtime/Extensions/LinqExtensions.cs
+++ Assets/UnityTools/Runtime/Extensions/LinqExtensions.cs
@@ -7,29 +7,27 @@ namespace UnityTools.Extentions
 {
     public static class LinqExtensions
     {
-        public static IEnumerable<T> NotNull<T>(this IEnumerable<T> ts)
-        {
-            return ts.Where(x => x != null);
-        }
-
         public static void ForEach<T>(this IEnumerable<T> ts, Action<T> action)
         {
-            var enumarator = ts.GetEnumerator();
-            while (enumarator.MoveNext())
+            foreach (var item in ts)
             {
-                action(enumarator.Current);
+                action(item);
             }
+        }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
+        {
+            int counter = 0;
+            foreach (var item in source)
+            {
+                action(item, counter++);
+            }
+            return source;
         }
 
         public static T GetRandom<T>(this IEnumerable<T> enumerable)
         {
-            var list = enumerable.ToList();
-            return list.ToArray()[Random.Range(0, list.Count)];
-        }
-
-        public static T GetRandom<T>(this List<T> list)
-        {
-            return list[Random.Range(0, list.Count)];
+            return enumerable.ElementAt(Random.Range(0, enumerable.Count()));
         }
 
         public static IEnumerable<T> OfType<T>(this IEnumerable<T> ts, Type type)
